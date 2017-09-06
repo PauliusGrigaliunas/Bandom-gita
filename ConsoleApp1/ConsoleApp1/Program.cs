5bd1a8d4a9ab61164e4bb639660e01b7a7e3a7b5
KaSaras <skbzuvis@gmail.com>
2017-09-06 18:45:21 +0300

    Organized code, added Mul function
    Added multiplication

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,23 +10,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int a, b;
+            int a, b, c;
             Console.WriteLine("Hi dudes, git works");
             a = Suma(2, 3);
             b = Sub(5, 3);
-            Console.WriteLine("a = {0}, b = {1}", a, b);
+            c = Mul(2, 6);
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
         }
         static int Suma (int a, int b)
         {
-            return a + b;
-           
-            
+            return a + b;    
         }
 
-        static int Sub(int a, int b) {
+        static int Sub(int a, int b)
+        {
             return a - b;
         }
 
-
+        static int Mul(int a, int b)
+        {
+            return a * b;
+        }
     }
 }
